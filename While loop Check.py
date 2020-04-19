80188d56a0b34e540dd83521480a0c0e2f393111
Aron <62036603+Aronace21@users.noreply.github.com>
2020-04-19 19:19:06 -0400

    Add files via upload

---
+++ While loop Check.py
@@ -0,0 +1,13 @@
+print("Hello what is your salary? I will double it until it gets to one million.")
+mySalary = int (input())
+# XD = int(mySalary)
+while mySalary <= 1000000:
+    XD = (mySalary * 2)
+    # int(newSalary)
+    # print (("Your salary is. " + (newSalary)
+    # print (newSalary)
+    # print(XD * 2 < 100)
+    # XD*=2
+    print (XD)
+    mySalary = XD
+# print (mySalary)
